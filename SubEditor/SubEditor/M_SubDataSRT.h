3c91f5a5e13122d91baf5461e7c2784df48b085b
lvalerom <lvaleromartin@gmail.com>
2013-11-21 19:38:18 +0100

    DATE

--- SubEditor/SubEditor/M_SubDataSRT.h
+++ SubEditor/SubEditor/M_SubDataSRT.h
@@ -1,8 +1,28 @@
+/*
+        Copyright 2013 Luis Valero Martin
+
+        This file is part of SubEditor.
+
+        SubEditor is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        SubEditor is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with SubEditor.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
+
+#include "M_SubDataClass.h"
 
 using namespace System;
 
-ref class M_SubDataSRT
+ref class M_SubDataSRT : public M_SubDataClass
 {
 public: ref struct nodeData{
 			 int ind;
